872225a51151976a28f710db90aa06953ba74033
Halyna <minimal005@gmail.com>
2023-11-23 21:53:32 +0200

    added class for card (OOP)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict'
 window.addEventListener('DOMContentLoaded', ()=>{
 
 // Tabs-----------------------------------------------------
@@ -94,6 +95,147 @@ function setClock(selector, endtime){
     }
 }
 setClock('.timer', deadLine)
+
+
+// Modal-----------------------------------------------------------
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close')
+
+function openModal (){
+    modal.classList.toggle('show')
+    document.body.style.overflow = 'hidden'
+    clearInterval(modalTimerId)
+}
+function closeModal(){
+    modal.classList.toggle('show')
+    document.body.style.overflow = ''
+}
+
+modalTrigger.forEach(btn => {
+    btn.addEventListener('click', ()=>{
+        openModal()
+})                                                                                                  
+})
+
+modalCloseBtn.addEventListener('click', closeModal)
+
+modal.addEventListener('click', (e)=>{
+    if(e.target === modal){
+        closeModal()
+    }
+})
+
+document.addEventListener('keydown', (e)=>{
+    if(e.code === 'Escape' && modal.classList.contains('show'))
+    closeModal()
+})
+
+// const modalTimerId = setTimeout(openModal, 5000)
+
+// Модалка вмикається, коли користувач доскролив до кінця сторінки
+function showModalByScroll(){
+    if(window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1){
+        openModal()
+        window.removeEventListener('scroll', showModalByScroll)
+    }
+}
+window.addEventListener('scroll', showModalByScroll)
+
+
+function sayName(surname){
+    console.log(this.name + ' ' + surname);
+}
+
+const obj = {
+    name: 'John'
+}
+
+sayName.call(obj, 'Smith')
+
+function count(num){
+    return this*num
+}
+
+const double = count.bind(2)
+console.log(double(5));
+
+const ob = {
+    num:5,
+    sayNumber(){
+        const say = () => {
+            console.log(this.num);
+        }
+        say()
+    }
+}
+ob.sayNumber()
+
+
+// Class для карток-------------------------------------------------------
+
+class MenuCard {
+    constructor(src, alt, title, description, price, parentSelector, ...classes){
+        this.src = src
+        this.alt = alt
+        this.title = title
+        this.description = description
+        this.price = price
+        this.transfer = 27
+        this.classes = classes
+        this.parent = document.querySelector(parentSelector)
+    }
+    changeToUAN(){
+        return this.price * this.transfer
+    }
+    render(){
+        const element = document.createElement('div')
+
+        if(this.classes.length === 0){
+            element.classList.add('menu__item')
+        } else {
+            this.classes.forEach(className => element.classList.add(className))
+        }
+        
+        element.innerHTML = `
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.description}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.changeToUAN()}</span> грн/день</div>
+            </div>`
+
+        this.parent.append(element)
+    }
+}
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        'vegy',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container',
+    ).render()
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        'elite',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        18,
+        '.menu .container',
+        'menu__item'
+    ).render()
+    new MenuCard(
+        "img/tabs/post.jpg",
+        'post',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        12,
+        '.menu .container',
+        'menu__item'
+    ).render()
 
 
 
